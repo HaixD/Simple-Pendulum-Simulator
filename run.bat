77e8dc9b87d3762b6bb636849fef84eaba2c7399
Hai <149478749+HaixD@users.noreply.github.com>
2023-11-01 00:13:48 -0700

    run.bat will try to install requirements.txt

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+pip install -r requirements.txt
 python generate_simulation.py 5 75 10 "simulation data"
 python plot_simulation.py "simulation data" "output" "simulation.npy"
 python data.py "simulation data" "output" "simulation.npy"
